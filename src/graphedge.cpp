e19019a7d72eb59e274ae4c50284aa656b24ce5a
Viktor Taranenko <viktortnk@gmail.com>
2022-10-05 12:04:08 +0100

    .clang-format. and reformatting

--- src/graphedge.cpp
+++ src/graphedge.cpp
@@ -1,22 +1,13 @@
-#include "graphnode.h"
 #include "graphedge.h"
 
-GraphEdge::GraphEdge(int id)
-{
-    _id = id;
+#include "graphnode.h"
+
+GraphEdge::GraphEdge(int id) { _id = id; }
+
+void GraphEdge::SetChildNode(GraphNode *childNode) { _childNode = childNode; }
+
+void GraphEdge::SetParentNode(GraphNode *parentNode) {
+  _parentNode = parentNode;
 }
 
-void GraphEdge::SetChildNode(GraphNode *childNode)
-{
-    _childNode = childNode;
-}
-
-void GraphEdge::SetParentNode(GraphNode *parentNode)
-{
-    _parentNode = parentNode;
-}
-
-void GraphEdge::AddToken(std::string token)
-{
-    _keywords.push_back(token);
-}
+void GraphEdge::AddToken(std::string token) { _keywords.push_back(token); }
